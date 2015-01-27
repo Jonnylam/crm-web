8c6f69cb96798e76d7ebfed0316f76889c9caa98
Jonathan Lam <jonathanryanlam@gmail.com>
2015-01-27 14:57:44 -0500

    first test

--- crm.rb
+++ crm.rb
@@ -1,16 +1,44 @@
-require 'sinatra'
 require './contact'
+require './rolodex'
+require 'sinatra'
+require 'sinatra/reloader'
+
+# set :server, :webrick
+
+disable :raise_errors
+disable :show_exceptions
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+error 500 do | exception |
+    "Fatal Error Occurred: #{ h exception }<br> <pre>#{h exception.backtrace.join("\n")}</pre>"
+end
+
+$rolodex = Rolodex.new
+
 get '/' do
   @crm_name = "My CRM"
   erb :index
 end
 
-
-get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
+get '/contacts' do
+  @contacts = $rolodex.contacts
   erb :contacts
 end
+
+get '/contacts/new' do
+  erb :new
+end
+
+post '/contacts' do
+  contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(contact)
+  redirect to('/contacts')
+end
+
+
+
