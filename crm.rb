724c8aa6e3eb772bde57562ac3627738ef088146
Jonathan Lam <jonathanryanlam@gmail.com>
2015-01-30 11:44:44 -0500

    .git ignore and delete contact.rb

--- crm.rb
+++ crm.rb
@@ -1,9 +1,42 @@
-require_relative 'contact'
+
 require_relative 'rolodex'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'data_mapper'
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+#database.sqlite3 is the name of your project
 
-  $rolodex = Rolodex.new
+class Contact
+  include DataMapper::Resource
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :email, String
+  property :note, String
+
+
+  #includes functionality of Datamapper
+  #module DataMapper
+  #   module Resource
+  #   end
+  #end
+  # attr_accessor :id, :first_name, :last_name, :email, :note
+  
+  # def initialize(first_name, last_name, email, note)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  #   @email = email
+  #   @note = note
+  # end
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+
+#End of data mapper setup
+
+$rolodex = Rolodex.new
 
 
 
@@ -51,16 +84,51 @@ disable :show_exceptions
   end
 
 
-  # get '/contacts/find' do
-  #   @title = "Find a contact"
-  #   erb :findcontact
-  # end
 
-  # post '/contacts/show_contact' do
-  #   @@rolodex.find(params[:id].to_i)
-  #   redirect to ('/contacts/:id')
-  # end
+  get '/contacts/find' do 
+    @title = "Find a contact"
+    puts "*****#{params.inspect}*****"
+    params[:search]
+    @contact = $rolodex.find(params[:search].to_i)
+    if @contact
+      #Do I need to redirect to the contact/:id page?
+      erb :show_contact
+    else
+      puts "noooo"
+    end
+    erb :findcontact
+  end
 
+
+# get '/contacts/findtherightcontact' do
+#   @contact = $rolodex.find(params[:search].to_i)
+#   erb :find
+# end
+
+
+# get '/contacts/search' do
+#   erb :findcontact
+# end
+
+
+# post '/contacts/search' do
+#   @contact = Contact.get(params[:id].to_i)
+#   if @contact
+#     erb :show_contact
+#   else
+#     put "noooo"
+#   end
+# end
+
+
+  # post '/contacts/:id' do
+  #   @contact = $rolodex.find(params[:search])
+  #   if @contact
+  #     erb :show_contact
+  #   else
+  #     raise Sinatra::NotFound
+  #   end
+  # end
 
 get "/contacts/:id" do
   @contact = $rolodex.find(params[:id].to_i)
